34bd33bc352004ca204a284894df15b570c6a791
yanjq <yanjq@realbull.net>
2018-09-30 11:14:37 +0800

    no message

---
+++ godot/autoload/global.gd
@@ -0,0 +1,27 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var current_scene = null
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func goto_scene(path):
+	 call_deferred("_deferred_goto_scene", path);
+
+func _deferred_goto_scene(path):
+	 current_scene.free()
+	 var s = ResourceLoader.load(path)
+	 current_scene = s.instance()
+	 get_tree().get_root().add_child(current_scene)
+	 get_tree().set_current_scene(current_scene)
---
+++ godot/robisplash/splash.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+#	var res = preload("res://assets/robisplash_logo.png")
+#	$Sprite.texture = res
+	var bullet = preload("res://splash.tscn").instance()
+	add_child(bullet)
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
